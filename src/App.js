c65bb99a4a7829cf64b7b7aceabc188ac982e215
anusesachin <jut.amazon.acc@gmail.com>
2024-01-15 11:03:25 +0530

    code added for redux

--- src/App.js
+++ src/App.js
@@ -2,26 +2,23 @@
 import React from 'react';
 import './App.css';
 import './global.css';
-
+import { Provider } from 'react-redux';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import LoginModule from './components/Login/LoginModule';
 import Sidebar from './components/Sidebar/Sidebar';
-import Dashboard from './components/Sidebar/Pages/Dashboard';
-import Header from './components/Sidebar/Header';
-import Drawer from './components/Sidebar/Drawer';
-import OtpForm from './components/Login/OtpForm';
-import LoginForm from './components/Login/LoginForm';
 
+import store from './store';
 const App = () => {
   return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/*' element={<LoginModule />} />
+          <Route path='/home/*' element={<Sidebar />} />
+        </Routes>
 
-    <BrowserRouter>
-    <Routes>
-      <Route path='/*' element={<LoginModule/>}/>
-      <Route path='/home/*' element={<Sidebar/>}/>
-    </Routes>
-    
-    </BrowserRouter>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
